5259378130cda6e4505801eb459b04d5ed89e268
Kishan Patel <kpatel20538@gmail.com>
2020-11-11 03:22:38 -0600

    feat: Implemented reducer that represents rpn and it's state transitsions

--- client/core/index.js
+++ client/core/index.js
@@ -1,5 +1,7 @@
 const WHITESPACE = /\s+/;
 
+const OPERATOR_ARTIY = 2;
+
 const operators = {
   "+": (a, b) => a + b,
   "-": (a, b) => a - b,
@@ -32,5 +34,36 @@ const parseOpcodes = (text) => {
   return opcodes;
 };
 
-export const reducer = (state, action) => null
-export const getInitialState = () => null;
+const reduceStackByOperator = (stack, opcode) => {
+  if (stack.length < OPERATOR_ARTIY) {
+    throw new Error(`Not enough arguments for operator: ${opcode.kind}`);
+  }
+
+  const operation = operators[opcode.kind];
+  const opcodes = stack.slice(stack.length - OPERATOR_ARTIY);
+  const args = opcodes.map((arg) => arg.value);
+  const result = operation(...args);
+
+  const nextOpcode = { type: "number", value: result };
+  const nextStack = stack.slice(0, stack.length - OPERATOR_ARTIY);
+
+  nextStack.push(nextOpcode);
+
+  return nextStack;
+};
+
+const reduceStackByOpcode = (stack, opcode) => {
+  const nextStack =
+    opcode.type === "operator"
+      ? reduceStackByOperator(stack, opcode)
+      : [...stack, opcode];
+  return nextStack;
+};
+
+export const reducer = (stack, action) => {
+  const opcodes = parseOpcodes(action.text);
+  const nextStack = opcodes.reduce(reduceStackByOpcode, stack);
+  return nextStack;
+};
+
+export const getInitialState = () => [];
