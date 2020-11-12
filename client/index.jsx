28e69324f7403e013e97a3e08f773c77d0d2e9e7
Kishan Patel <kpatel20538@gmail.com>
2020-11-12 04:01:09 -0600

    refactor: Moving App component out of index file

--- client/index.jsx
+++ client/index.jsx
@@ -1,57 +1,7 @@
-import React, { useReducer, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import "bulma/css/bulma.css";
-
-import { getInitialState, reducer } from "./core";
-import ErrorLog from "./component/ErrorLog";
-
-const App = () => {
-  const [state, dispatch] = useReducer(reducer, getInitialState());
-  const [input, setInput] = useState("");
-
-  const stack = state.value;
-
-  return (
-    <div className="container">
-      <div className="section">
-        <div className="field is-grouped">
-          <div className="control is-expanded">
-            <input
-              className="input"
-              type="text"
-              placeholder="Enter Input..."
-              value={input}
-              onChange={(event) => {
-                setInput(event.target.value);
-              }}
-            />
-          </div>
-          <div className="control">
-            <button
-              className="button is-info"
-              onClick={() => {
-                setInput("");
-                dispatch({ type: "input", text: input });
-              }}
-            >
-              +
-            </button>
-          </div>
-        </div>
-        <div className="columns">
-          <div className="column is-two-thirds">
-            <div className="box">
-              <pre>{stack.map((opcode) => opcode.value).join(", ")}</pre>
-            </div>
-          </div>
-          <div className="column is-one-thirds">
-            <ErrorLog errors={state.errors} dispatch={dispatch} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+import App from "./component/App";
 
 const container = document.getElementById("root");
 ReactDOM.render(<App />, container);
